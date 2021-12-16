08e31d765a13a13d371593e164870fed9c96ee86
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-15 22:05:20 -0300

    Feat: add @Entity annotation to Product.java

--- src/main/java/com/ndrewcoding/Product.java
+++ src/main/java/com/ndrewcoding/Product.java
@@ -4,9 +4,11 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Entity;
 import java.math.BigDecimal;
 import java.util.Date;
 
+@Entity
 public class Product extends PanacheEntity {
 
     private String name;
