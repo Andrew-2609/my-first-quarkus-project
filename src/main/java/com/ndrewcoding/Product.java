9f06cef84d619394f2f5ca7879d23aeb633d2a3a
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-12-15 22:04:56 -0300

    Feat: add implementation of Product as a subclass of PanacheEntity

--- src/main/java/com/ndrewcoding/Product.java
+++ src/main/java/com/ndrewcoding/Product.java
@@ -1,2 +1,43 @@
-package com.ndrewcoding;public class Product {
+package com.ndrewcoding;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Product extends PanacheEntity {
+
+    private String name;
+    private BigDecimal value;
+    @CreationTimestamp
+    private Date creationDate;
+    @UpdateTimestamp
+    private Date updateDate;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
 }
